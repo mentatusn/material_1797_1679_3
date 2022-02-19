39fbafaea4d1b281f5375c2a11641f1b4067fd9e
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-19 11:42:10 +0300

    урок 1 начали

---
+++ app/src/main/java/com/gb/material_1797_1679_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gb.material_1797_1679_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
