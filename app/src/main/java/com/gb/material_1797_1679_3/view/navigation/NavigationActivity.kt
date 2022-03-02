7a0dd0beb34d7f432701bdec3a6151d7c070c409
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-02 21:29:48 +0300

    урок 3 добавили customView для TabItem

--- app/src/main/java/com/gb/material_1797_1679_3/view/navigation/NavigationActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/navigation/NavigationActivity.kt
@@ -10,12 +10,23 @@ import com.gb.material_1797_1679_3.view.main.PictureOfTheDayFragment
 
 class NavigationActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityNavigationBinding
+    lateinit var binding: ActivityNavigationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+        /*binding.tabLayout.getTabAt(EARTH_KEY)?.setIcon(R.drawable.ic_earth)
+        binding.tabLayout.getTabAt(MARS_KEY)?.setIcon(R.drawable.ic_mars)
+        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setIcon(R.drawable.ic_system)*/
+
+        binding.tabLayout.getTabAt(EARTH_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_earth)
+        binding.tabLayout.getTabAt(MARS_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_mars)
+        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_system)
+
+
     }
 
 }
--- app/src/main/java/com/gb/material_1797_1679_3/view/navigation/ViewPagerAdapter.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/navigation/ViewPagerAdapter.kt
@@ -4,15 +4,26 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
-private const val EARTH_KEY=0
-private const val MARS_KEY=1
-private const val SYSTEM_KEY=2
+const val EARTH_KEY = 0
+const val MARS_KEY = 1
+const val SYSTEM_KEY = 2
 
-class ViewPagerAdapter(private val fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
+class ViewPagerAdapter(private val fragmentManager: FragmentManager) :
+    FragmentStatePagerAdapter(fragmentManager) {
 
-    private val fragments = arrayOf(EarthFragment(),MarsFragment(),SystemFragment())
+    private val fragments = arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
 
-    override fun getCount()= fragments.size
+    override fun getCount() = fragments.size
 
     override fun getItem(position: Int) = fragments[position]
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            EARTH_KEY -> "Earth"
+            MARS_KEY -> "Mars"
+            SYSTEM_KEY -> "System"
+            else -> "Earth"
+        }
+    }
+
 }
