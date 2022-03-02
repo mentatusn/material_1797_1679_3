6374bdc799b19ce1e48540e4fc888b769e1d8503
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-02 22:54:39 +0300

    урок 3 закончили, факультатив - миграция с ViewPager на ViewPager2

--- app/src/main/java/com/gb/material_1797_1679_3/view/navigation/NavigationActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/navigation/NavigationActivity.kt
@@ -7,6 +7,8 @@ import android.widget.TextView
 import com.gb.material_1797_1679_3.R
 import com.gb.material_1797_1679_3.databinding.ActivityNavigationBinding
 import com.gb.material_1797_1679_3.view.main.PictureOfTheDayFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class NavigationActivity : AppCompatActivity() {
 
@@ -15,16 +17,25 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout,binding.viewPager
+        ) { tab, position ->
+            tab.text = when (position) {
+                EARTH_KEY -> "Earth"
+                MARS_KEY -> "Mars"
+                SYSTEM_KEY -> "System"
+                else -> "Earth"
+            }
+        }.attach()
 
         /*binding.tabLayout.getTabAt(EARTH_KEY)?.setIcon(R.drawable.ic_earth)
         binding.tabLayout.getTabAt(MARS_KEY)?.setIcon(R.drawable.ic_mars)
         binding.tabLayout.getTabAt(SYSTEM_KEY)?.setIcon(R.drawable.ic_system)*/
 
-        binding.tabLayout.getTabAt(EARTH_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_earth)
+       /* binding.tabLayout.getTabAt(EARTH_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_earth)
         binding.tabLayout.getTabAt(MARS_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_mars)
-        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_system)
+        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_system)*/
 
 
     }
--- app/src/main/java/com/gb/material_1797_1679_3/view/navigation/ViewPagerAdapter.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/navigation/ViewPagerAdapter.kt
@@ -1,29 +1,23 @@
 package com.gb.material_1797_1679_3.view.navigation
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 const val EARTH_KEY = 0
 const val MARS_KEY = 1
 const val SYSTEM_KEY = 2
 
-class ViewPagerAdapter(private val fragmentManager: FragmentManager) :
-    FragmentStatePagerAdapter(fragmentManager) {
+class ViewPagerAdapter(private val fragmentManager: FragmentActivity) :
+    FragmentStateAdapter(fragmentManager) {
 
     private val fragments = arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
 
-    override fun getCount() = fragments.size
 
-    override fun getItem(position: Int) = fragments[position]
+    override fun getItemCount() = fragments.size
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            EARTH_KEY -> "Earth"
-            MARS_KEY -> "Mars"
-            SYSTEM_KEY -> "System"
-            else -> "Earth"
-        }
-    }
+    override fun createFragment(position: Int)= fragments[position]
 
 }
