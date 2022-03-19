4ae46d863388d57a1938b6c71b968450ce64fb22
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-19 13:38:36 +0300

    урок 5 познакомились с ChangeImageTranform( изменяли размеры картинки)

--- app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
@@ -7,6 +7,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
@@ -14,59 +15,28 @@ import com.gb.material_1797_1679_3.R
 
 import com.gb.material_1797_1679_3.databinding.ActivityAnimationsBinding
 import com.gb.material_1797_1679_3.databinding.ActivityAnimationsExplodeBinding
+import com.gb.material_1797_1679_3.databinding.ActivityAnimationsZoomBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityAnimationsExplodeBinding
+    lateinit var binding: ActivityAnimationsZoomBinding
     var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsExplodeBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsZoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imageView.setOnClickListener {
 
-        binding.recyclerView.adapter =Adapter()
+            val changeBounds = ChangeImageTransform()
+            changeBounds.duration = 3000
+            TransitionManager.beginDelayedTransition(binding.container,changeBounds)
+            flag = !flag
+            binding.imageView.scaleType = if(flag) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.CENTER_INSIDE
+        }
     }
 
-    private fun explode(clickedView: View){
-        val set = TransitionSet()
-        val explode = Explode()
-        val fade = Fade()
-        fade.duration = 3000
-        var rect = Rect()
-        clickedView.getGlobalVisibleRect(rect)
-        explode.duration = 2000
-        explode.excludeTarget(clickedView,true)
-        set.addTransition(explode)
-        set.addTransition(fade)
-        explode.epicenterCallback = object: Transition.EpicenterCallback(){
-            override fun onGetEpicenter(transition: Transition): Rect {
-                return rect
-            }
-        }
-        TransitionManager.beginDelayedTransition(binding.recyclerView,set)
-        binding.recyclerView.adapter =null
-    }
 
-     inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.activity_animations_explode_recycler_item,
-                    parent,
-                    false
-                ) as View
-            )
-        }
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.itemView.setOnClickListener {
-                explode(it)
-            }
-        }
-        override fun getItemCount(): Int {
-            return 32
-        }
-    }
-     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
 
 
 }
