e306eeaa31e470e1bccf12e9cbdd477281a824fc
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-19 14:05:03 +0300

    урок 5 познакомились с концепцией псевдонимов ViewCompat.setTransitionName

--- app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
@@ -10,34 +10,37 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.gb.material_1797_1679_3.R
-
-import com.gb.material_1797_1679_3.databinding.ActivityAnimationsBinding
-import com.gb.material_1797_1679_3.databinding.ActivityAnimationsExplodeBinding
-import com.gb.material_1797_1679_3.databinding.ActivityAnimationsPathBinding
-import com.gb.material_1797_1679_3.databinding.ActivityAnimationsZoomBinding
+import com.gb.material_1797_1679_3.databinding.*
 
 class AnimationsActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityAnimationsPathBinding
+    lateinit var binding: ActivityAnimationsShuffleBinding
     var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsPathBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsShuffleBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
 
-            val changeBounds = ChangeBounds()
-            changeBounds.duration= 5000
-            changeBounds.setPathMotion(ArcMotion())
-            TransitionManager.beginDelayedTransition(binding.root,changeBounds)
-            flag = !flag
-            val params = binding.button.layoutParams as FrameLayout.LayoutParams
-            params.gravity = if(flag) Gravity.BOTTOM or Gravity.END else Gravity.TOP or Gravity.START
-            binding.button.layoutParams = params
+        val titles: MutableList<String> = ArrayList()
+        repeat(5) {
+            titles.add("item $it")
+        }
+        binding.button.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
+            titles.shuffle()
+            binding.transitionsContainer.removeAllViews()
+            titles.forEach { title:String->
+                binding.transitionsContainer.addView(TextView(this).apply {
+                    text = title
+                    ViewCompat.setTransitionName(this,title)
+                })
+            }
         }
     }
 
