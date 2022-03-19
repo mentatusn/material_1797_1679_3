c453837d4adb3b0687949d7b0ec3f1e3581ebaa8
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-19 14:48:08 +0300

    урок 5 разобрались с stateListAnimator

--- app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/animations/AnimationsActivity.kt
@@ -32,63 +32,8 @@ class AnimationsActivity : AppCompatActivity() {
         binding = ActivityAnimationsRotateFabBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.fab.setOnClickListener {
-            flag = !flag
-            if(flag){
-                ObjectAnimator.ofFloat( binding.plusImageview,View.ROTATION,0f,405f).setDuration(duration).start()
-                ObjectAnimator.ofFloat( binding.optionOneContainer,View.TRANSLATION_Y,-50f,-260f).setDuration(duration).start()
-                ObjectAnimator.ofFloat( binding.optionTwoContainer,View.TRANSLATION_Y,-20f,-130f).setDuration(duration).start()
-
-                binding.optionOneContainer.animate()
-                    .alpha(1f)
-                    .setDuration(duration/2)
-                    .setListener(object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            super.onAnimationEnd(animation)
-                            binding.optionOneContainer.isClickable = true
-                        }
-                    })
-                binding.optionTwoContainer.animate()
-                    .alpha(1f)
-                    .setDuration(duration/2)
-                    .setListener(object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            super.onAnimationEnd(animation)
-                            binding.optionTwoContainer.isClickable = true
-                        }
-                    })
-
-                binding.transparentBackground.animate()
-                    .alpha(0.5f)
-                    .setDuration(duration)
-            }else{
-                ObjectAnimator.ofFloat( binding.plusImageview,View.ROTATION,405f,0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat( binding.optionOneContainer,View.TRANSLATION_Y,-260f,-50f).setDuration(duration).start()
-                ObjectAnimator.ofFloat( binding.optionTwoContainer,View.TRANSLATION_Y,-130f,-20f).setDuration(duration).start()
-
-                binding.optionOneContainer.animate()
-                    .alpha(0f)
-                    .setDuration(duration/2)
-                    .setListener(object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            super.onAnimationEnd(animation)
-                            binding.optionOneContainer.isClickable = false
-                        }
-                    })
-                binding.optionTwoContainer.animate()
-                    .alpha(0f)
-                    .setDuration(duration/2)
-                    .setListener(object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            super.onAnimationEnd(animation)
-                            binding.optionTwoContainer.isClickable = false
-                        }
-                    })
-                binding.transparentBackground.animate()
-                    .alpha(0f)
-                    .setDuration(duration)
-
-            }
+        binding.scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            binding.header.isSelected = binding.scrollView.canScrollVertically(-1)
         }
     }
 
