44958b23be685a62e701ece1aee0429f6c9aeb41
Andej Nesterenko <mentatus.n@gmail.com>
2022-03-30 21:11:21 +0300

    урок 8 разобрали правильный подход в работе с текстами в material

--- app/src/main/java/com/gb/material_1797_1679_3/view/ux/UXActivity.kt
+++ app/src/main/java/com/gb/material_1797_1679_3/view/ux/UXActivity.kt
@@ -29,6 +29,8 @@ class UXActivity : AppCompatActivity() {
             }
             true
         }
+
+        binding.bottomNavigationViewUX.selectedItemId = R.id.fragment_ux
     }
 
     fun navigateTo(fragment: Fragment){
